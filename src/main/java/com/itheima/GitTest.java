5ed9bbc1179541eecfc117f65cae28d1d117014c
“hbn” <1029426691@qq.com>
2022-07-30 23:51:09 +0800

    第12次提交

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hot-fix test...");
         System.out.println("hot-fix test...");
         System.out.println("hot-fix test...");
-        System.out.println("hot-fix test...");
+
     }
 }
