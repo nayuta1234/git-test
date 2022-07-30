c0e373b4c146f0353bc2622c72105ef2ca70bbfc
“hbn” <1029426691@qq.com>
2022-07-30 23:42:00 +0800

    fix-hot   提交

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test...");
         System.out.println("hot-fix test...");
+        System.out.println("hot-fix test...");
     }
 }
