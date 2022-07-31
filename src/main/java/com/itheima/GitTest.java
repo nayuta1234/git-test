2835e0ba2e85983d02e14b9d8c9c513381741002
hbn <1029426691@qq.com>
2022-07-31 03:39:05 +0000

    update src/main/java/com/itheima/GitTest.java. dasdasdasdas

--- src/main/java/com/itheima/GitTest.java
+++ src/main/java/com/itheima/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hot-fix  fix 提交3...");
         System.out.println("hot-fix  fix 提交4...");
         System.out.println("hot-fix  fix 提交5...");
+         System.out.println("hot-fix  fix 提交6...");
     }
 }
